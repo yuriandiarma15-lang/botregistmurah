93216125099578078a0f61df3c6bd8ddbb1b71f5
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-09-10 13:05:01 +0700

    Create packages.py

---
+++ packages.py
@@ -0,0 +1,15 @@
+PACKAGE_MAP = {
+
+    "4 hari": {
+        "label": "4 hari",
+        "price": 34000,
+        "days": 4
+    },
+
+    "1BLN": {
+        "label": "1 Bulan",
+        "price": 149000,
+        "days": 30
+    }
+
+}
